ae98c437acb5ba3deb0a0703ef8342db8594acb4
Kane <44578147+kingchou007@users.noreply.github.com>
2023-07-08 01:02:46 -0400

    update Navigation

--- swarm_controller/control_api.py
+++ swarm_controller/control_api.py
@@ -182,7 +182,28 @@ def left():
         for i in range(9):
             name_i = "UAV" + str(i + 1)
             client.moveByVelocityZAsync(v_cmd[0, i], v_cmd[1, i], z_cmd, 0.1, vehicle_name=name_i)
-
+        
+        # Navigation methods
+        # def up(self, desired_height = 30, speed = 10):
+        #     # update z_cmd to be the average of all UAVs' z position
+        #     z_cmd = self.controller.get_avg_altitude() - desired_height
+            
+        #     for i in range(self.num_uavs):
+        #         name_i = "UAV" + str(i + 1)
+        #         if i != self.num_uavs - 1:
+        #             self.client.moveToZAsync(z_cmd, speed, vehicle_name=name_i)
+        #         else:
+        #             self.client.moveToZAsync(z_cmd, speed, vehicle_name=name_i).join()  
+                    
+        # def down(self, desired_height = 20, speed = 10):
+        #     z_cmd = self.controller.get_avg_altitude() + desired_height
+            
+        #     for i in range(self.num_uavs):
+        #         name_i = "UAV" + str(i + 1)
+        #         if i != self.num_uavs - 1:
+        #             self.client.moveToZAsync(z_cmd, speed, vehicle_name=name_i)
+        #         else:
+        #             self.client.moveToZAsync(z_cmd, speed, vehicle_name=name_i).join()
 
 def right():
     v_max = 5
--- swarm_controller/controller/navigation.py
+++ swarm_controller/controller/navigation.py
@@ -19,27 +19,6 @@ class Navigation:
     def right(self, desired_distance = 10):
         self.controller.move_in_direction(np.array([[0], [desired_distance]]))
         
-        # Navigation methods
-    # def up(self, desired_height = 30, speed = 10):
-    #     # update z_cmd to be the average of all UAVs' z position
-    #     z_cmd = self.controller.get_avg_altitude() - desired_height
-        
-    #     for i in range(self.num_uavs):
-    #         name_i = "UAV" + str(i + 1)
-    #         if i != self.num_uavs - 1:
-    #             self.client.moveToZAsync(z_cmd, speed, vehicle_name=name_i)
-    #         else:
-    #             self.client.moveToZAsync(z_cmd, speed, vehicle_name=name_i).join()  
-                
-    # def down(self, desired_height = 20, speed = 10):
-    #     z_cmd = self.controller.get_avg_altitude() + desired_height
-        
-    #     for i in range(self.num_uavs):
-    #         name_i = "UAV" + str(i + 1)
-    #         if i != self.num_uavs - 1:
-    #             self.client.moveToZAsync(z_cmd, speed, vehicle_name=name_i)
-    #         else:
-    #             self.client.moveToZAsync(z_cmd, speed, vehicle_name=name_i).join()
     def up(self, desired_height = 30, speed = 10):
         z_cmd = self.controller.get_avg_altitude() - desired_height
         self.controller.execute_for_all_uavs(lambda name: self.controller.client.moveToZAsync(z_cmd, speed, vehicle_name=name))
@@ -47,14 +26,3 @@ class Navigation:
     def down(self, desired_height = 20, speed = 10):
         z_cmd = self.controller.get_avg_altitude() + desired_height
         self.controller.execute_for_all_uavs(lambda name: self.controller.client.moveToZAsync(z_cmd, speed, vehicle_name=name))
-
-                
-def main():
-    nav = Navigation()
-    nav.up()
-    # nav.down()
-
-                    
-
-if __name__ == "__main__":
-    main()
